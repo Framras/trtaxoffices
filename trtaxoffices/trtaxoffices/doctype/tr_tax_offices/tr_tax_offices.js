26690fc63295aafaf0161454f026397cf893008b
tufan@kaynak.gen.tr <!r0nM4!d3n!>
2019-11-03 15:07:11 +0300

    initiation

---
+++ trtaxoffices/trtaxoffices/doctype/tr_tax_offices/tr_tax_offices.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, Framras AS-Izmir and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('TR Tax Offices', {
+	// refresh: function(frm) {
+
+	// }
+});
