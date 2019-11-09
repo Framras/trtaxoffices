d12aed6147027959ae61ce8fa698366f61c5d50e
tufan@kaynak.gen.tr <!r0nM4!d3n!>
2019-11-09 20:54:49 +0300

    initiation

---
+++ trtaxoffices/trtaxoffices/doctype/tr_invoice_type/tr_invoice_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, Framras AS-Izmir and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('TR Invoice Type', {
+	// refresh: function(frm) {
+
+	// }
+});
